ae6c2c306364f4ac06bc45bcaecccc718cffe682
mikechao <mike.chao.one@gmail.com>
2025-04-11 16:21:32 -0700

    Eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
 
 export default antfu({
   formatters: true,
-})
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: 'single',
+  },
+});
